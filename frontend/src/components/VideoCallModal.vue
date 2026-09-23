da9a00ebb09cff6ecfff9dcc65b709fab7e64344
MilkDev-bit <miltonpinamoreno@outlook.com>
2026-09-23 15:59:43 -0600

    Bbw27bw

--- frontend/src/components/VideoCallModal.vue
+++ frontend/src/components/VideoCallModal.vue
@@ -51,9 +51,9 @@ onMounted(() => {
 function initJitsi() {
   if (!jitsiContainer.value) return
 
-  const options = {
+  const options: any = {
     roomName: props.roomName,
-    jwt: props.jwt,
+    ...(props.domain !== 'meet.jit.si' && props.jwt ? { jwt: props.jwt } : {}),
     width: '100%',
     height: '100%',
     parentNode: jitsiContainer.value,
