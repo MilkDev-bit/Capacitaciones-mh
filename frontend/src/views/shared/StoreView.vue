c7b4ffc747a2038bfba71405b2acceee75dd8fad
MilkDev-bit <miltonpinamoreno@outlook.com>
2026-09-24 12:26:47 -0600

    n82n8nw

--- frontend/src/views/shared/StoreView.vue
+++ frontend/src/views/shared/StoreView.vue
@@ -707,7 +707,7 @@ onUnmounted(() => window.removeEventListener('keydown', onKeydown))
                   <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
                   <circle cx="12" cy="10" r="3" />
                 </svg>
-                <span>Ciudad de México, México</span>
+                <span>León Guanajuato, México</span>
               </li>
             </ul>
           </div>
